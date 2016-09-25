fdc4cd3d153e0f93194dfdbc17f443bf4529fd28
ingvar73 <ingvar73@gmail.com>
2016-09-25 18:20:10 +0300

    Create/Delete

--- index.php
+++ index.php
@@ -36,6 +36,49 @@ $app->get('/user/{id}', function (ServerRequestInterface $req, ResponseInterface
     return $resp->withJson($data)->withHeader('Content-Type', 'application/json');
 });
 
+$app->get('/order', function (ServerRequestInterface $req, ResponseInterface $resp) use ($pdo){
+    $selectStatement = $pdo->select()->from('orders')->leftJoin('users', 'orders.customer_id', '=', 'users.id');
 
+    $stmt = $selectStatement->execute();
+    $data = $stmt->fetchAll();
+    return $resp->withJson($data)->withHeader('Content-Type', 'application/json');
+});
+
+$app->get('/order/{id}', function (ServerRequestInterface $req, ResponseInterface $resp) use ($pdo){
+    $order_id = $req->getAttribute('id');
+
+    $selectStatement = $pdo->select()->from('orders')->leftJoin('users', 'orders.customer_id', '=', 'users.id')->where('orders.id', '=', $order_id);
+
+    $stmt = $selectStatement->execute();
+    $data = $stmt->fetch();
+    return $resp->withJson($data)->withHeader('Content-Type', 'application/json');
+});
+
+$app->post('/user', function (ServerRequestInterface $req, ResponseInterface $resp) use ($pdo){
+    $data = $_POST;
+    $action = $data['action'];
+
+    switch ($action){
+        case 'create':
+                $statement = $pdo->insert(array('login', 'name'))
+                    ->into('users')
+                    ->values(array($data['login'], $data['name']));
+            break;
+        case 'delete':
+
+            break;
+    }
+
+    $stmt = $statement->execute();
+    if ($stmt){
+        $success = true;
+    } else{
+        $success = false;
+    }
+
+    $data = array('success' => $success);
+    return $resp->withJson($data)->withHeader('Content-Type', 'application/json');
+
+});
 
 $app->run();
