8608fe202cce05693f5edd155862be9eaf034c0b
ingvar73 <ingvar73@gmail.com>
2016-09-29 12:12:19 +0300

    JSON+AJAX

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@
 function select_table_del () {
     $('#sel_tab_del').change(function () {
         var selectedOption = $('#sel_tab_del option:selected');
+        return selectedOption;
     });
 }
 
@@ -17,54 +18,56 @@ function select_table_del () {
 //     document.forms[0].submit();
 // }
 
+$(document).ready(function () {
+    $.("#btn_delete").click(function () {
+        alert ("В функции");
+        var id = $("#id").val();
+        var action = $("#action").val();
 
-$.(".btn_delete").click(function () {
-      var id = $("#id").val();
-      var action = $("#action").val();
-    sel = select_table_del();
-    switch (sel){
-        case 'users':{
-        $('#myForm1').setAttribute('action', '/users');
-            $.ajax({
-                url: '/users',
-                type: 'POST',
-                dataType: 'json',
-                data: {id: id, action: delete},
-                success: function(data)
-                {
-                    console.log(data);
-                }
-            });
-            break;
-        }
-    case 'orders': {
-        $('#myForm1').setAttribute('action', '/orders');
-        $.ajax(
-            {
-            url: '/orders',
-            type: 'POST',
-            dataType: 'json',
-            data: {id: id, action: delete},
-            success: function(data)
-            {
-                console.log(data);
+        switch (select_table_del()){
+            case 'users':{
+                $("#myForm1").attr('action', '/users');
+                $.ajax({
+                    url: '/users',
+                    type: 'POST',
+                    dataType: 'json',
+                    data: {id: id, action: delete},
+                    success: function(data)
+                    {
+                        alert("Это USERS");
+                    }
+                });
+                break;
             }
-        });
-        break;
+            case 'orders': {
+                $("#myForm1").attr('action', '/orders');
+                $.ajax(
+                    {
+                        url: '/orders',
+                        type: 'POST',
+                        dataType: 'json',
+                        data: {id: id, action: delete},
+                        success: function(data)
+                        {
+                            alert("Это ORDERS");
+                        }
+                    });
+                break;
+            }
+            case 'products': {
+                $("#myForm1").attr('action', '/products');
+                $.ajax({
+                    url: '/products',
+                    type: 'POST',
+                    dataType: 'json',
+                    data: {id: id, action: delete},
+                    success: function(data)
+                    {
+                        alert("Это PRODUCTS");
+                    }
+                });
+                break;
+            }
         }
-    case 'products': {
-        $('#myForm1').setAttribute('action', 'products');
-            $.ajax({
-                url: '/products',
-                type: 'POST',
-                dataType: 'json',
-                data: {id: id, action: delete},
-                success: function(data)
-                {
-                    console.log(data);
-                }
-        });
-            break;
-        }
-    }
-   });
+    });
+});
